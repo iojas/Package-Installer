1e564226aeaad20d55fd60e927e38446bb1c4016
ojas <ojas@OJASPC>
2016-08-29 10:52:41 -0500

    I am commiting V1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] givenArray = { "A:B" };
+            HashSet<String> packages = new HashSet<string>();
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+
+            HashSet<string> dependent = new HashSet<string>();
+            HashSet<string> independent = new HashSet<string>();
+        }
+    }
+}
