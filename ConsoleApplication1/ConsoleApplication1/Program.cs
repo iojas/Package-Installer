12c75fd2d9efcdc7e8650100457284e205f703a9
ojas <ojas@OJASPC>
2016-08-29 11:05:27 -0500

    Able to identify dependents and independents correctly

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,14 +10,58 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            String[] givenArray = { "A:B" };
-            HashSet<String> packages = new HashSet<string>();
+            // Input should be given here.
+            String[] givenArray = { "KittenService: ",
+
+"Leetmeme: Cyberportal",
+
+"Cyberportal: Ice",
+
+"CamelCaser: KittenService",
+
+"Fraudstream: Leetmeme",
+
+"Ice: " };
 
             Dictionary<string, string> dict = new Dictionary<string, string>();
-
-
+            
+            // dependent are those who are on the Left side of each dependency, independent on right side. 
             HashSet<string> dependent = new HashSet<string>();
             HashSet<string> independent = new HashSet<string>();
+
+            foreach (String s in givenArray)
+            {
+                String[] packinList = s.Split(':');
+                if (!packinList[1].Trim().Equals(string.Empty))
+                {
+                    // Before adding to dicctionary remove the white spaces.
+                    independent.Add(packinList[1].Trim());
+                    dependent.Add(packinList[0].Trim());
+                    dict.Add(packinList[0].Trim(), packinList[1].Trim());
+                }
+                else
+                {
+                    independent.Add(packinList[0].Trim());
+                }
+
+            }
+            foreach (string x in dependent)
+            {
+                independent.Remove(x);
+            }
+            Console.WriteLine("Independents");
+            foreach (string x in independent)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dependents");
+            foreach (string x in dependent)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
         }
     }
 }
